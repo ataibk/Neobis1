2dd65453458b84686b7221f2d6f5f9161f9af9bb
ataibk <ataibk25@gmail.com>
2019-01-28 06:18:22 +0600

    Add files via upload

---
+++ Task2.2/ConsoleApplication5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Task2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите а: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+   
+            Console.Write("Введите c: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine($"{a} eh o maior");
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine($"{b} eh o maior");
+            }
+            else
+            {
+                Console.WriteLine($"{c} eh o maior");
+            }
+            double MaiorAB = (a + b + Math.Abs(a - b)) / 2;
+
+            Console.WriteLine($"{MaiorAB} ");
+            Console.ReadKey();
+        }
+    }
+}
