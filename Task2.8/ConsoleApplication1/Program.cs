11aa3b7407c79ea5f4850891267a87e9f03832ea
ataibk <ataibk25@gmail.com>
2019-01-28 06:33:14 +0600

    Add files via upload

---
+++ Task2.8/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Task2_8
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter text from 1 to 500: ");
+            string text = Console.ReadLine();
+            if (text.Length >= 500 || text.Length <= 1)
+            { }
+            if (text.Length > 80)
+            {
+                Console.WriteLine("No");
+            }
+            else Console.WriteLine("Yes");
+            Console.ReadKey();
+        }
+    }
+}
