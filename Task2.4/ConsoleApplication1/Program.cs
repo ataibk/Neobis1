72c5ea861e73ff7f970ec956038edc89fc92c70f
ataibk <ataibk25@gmail.com>
2019-01-28 06:30:38 +0600

    Add files via upload

---
+++ Task2.4/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert X");
+            int x = int.Parse(Console.ReadLine());
+            if (x >= 1 && x <= 1000)
+            {
+                for (int i = 0; i <= x; i++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+             }
+            Console.ReadKey();
+        }
+    }
+}
