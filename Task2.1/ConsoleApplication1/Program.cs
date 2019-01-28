17278169972ad1956922ea15d2f744910f03d145
ataibk <ataibk25@gmail.com>
2019-01-28 06:17:03 +0600

    Add files via upload

---
+++ Task2.1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Количество рабочих:");
+            int NoW = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Количество рабочих часов в 1 месяце:");
+            int HfM = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Зарплата за час:");
+            double PpH = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"NUMBER = {NoW}");
+            Console.WriteLine($"SALARY = {Salary(HfM, PpH)}");
+
+            Console.ReadKey();
+        }
+        static double Salary(int a, double b)
+        {
+            double c = a * b;
+            return c;
+        }
+    }
+}
