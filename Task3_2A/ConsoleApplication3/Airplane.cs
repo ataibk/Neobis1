29e7a08799c2be91c3c1ab0321b9a1c92b1ab139
ataibk <ataibk25@gmail.com>
2019-02-20 14:01:13 +0600

    Add files via upload

---
+++ Task3_2A/ConsoleApplication3/Airplane.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Airplane : Vehicle
+    {
+        public void debug()
+        {
+            Name = "Boeing";
+            Weight = "500t";
+            averageSpeed = "950km/h";
+            powerEngine = "2000h/p";
+
+            Console.WriteLine("\nAirplane:");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Name);
+            Console.WriteLine(Weight);
+            Console.WriteLine(averageSpeed);
+            Console.WriteLine(powerEngine);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Task3_2A/ConsoleApplication3/Automobile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Automobile : Vehicle
+    {
+        public void debug()
+        {
+            Name = "BMW";
+            Weight = "1500kg";
+            averageSpeed = "320km/h";
+            powerEngine = "900h/p";
+
+            Console.WriteLine("Automobile:");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Name);
+            Console.WriteLine(Weight);
+            Console.WriteLine(averageSpeed);
+            Console.WriteLine(powerEngine);
+            Console.ResetColor();
+        }
+    }
+}
